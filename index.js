dd79bef1564607483b7233da90ecaec6b8e82c4a
William Damm <wdamm@powerhrg.com>
2018-12-27 03:09:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var name = "joe"
-var height = 74
-var message = `${name} is ${height} inches tall`
-
-module.exports = {name, height, message}
+    var name = "Joe"
+    var height = "74"
+    var message = `${name} is ${height} inches tall`
+     
+    module.exports = {name, height, message}
